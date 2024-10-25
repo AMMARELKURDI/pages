b98e3691f0c1139cec0fce483c979117774e2c33
AMMARELKURDI <155187194+AMMARELKURDI@users.noreply.github.com>
2024-10-25 21:41:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Welcome",
+    page_icon=":clap:"
+)
+st.write("# Welcome to Streamlit :clap: ")
+st.sidebar.success("Select Demo Above")
